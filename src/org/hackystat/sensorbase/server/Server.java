6d524950c7854e442cff1799f10c383ed29556a0
Philip Johnson <philipmjohnson@gmail.com>
2007-05-12 02:00:16 +0000

    Issue: [http://code.google.com/p/hackystat-sensorbase-uh/issues/detail?id=3]
    Added initial Resource classes.

--- src/org/hackystat/sensorbase/server/Server.java
+++ src/org/hackystat/sensorbase/server/Server.java
@@ -1,73 +1,40 @@
 package org.hackystat.sensorbase.server;
 
+import org.restlet.Application;
 import org.restlet.Component;
 import org.restlet.Restlet;
-import org.restlet.data.Form;
-import org.restlet.data.MediaType;
-import org.restlet.data.Method;
-import org.restlet.data.Parameter;
+import org.restlet.Router;
 import org.restlet.data.Protocol;
-import org.restlet.data.Request;
-import org.restlet.data.Response;
-import org.restlet.data.Status;
+
 
 /**
- * The Server class. 
+ * Sets up the HTTP Server process and dispatching to the associated resources. 
  * @author Philip Johnson
  */
-public class Server {
+public class Server extends Application {
   
   /**
-   * The main program that is invoked when running the SensorBase from the command line. 
-   * @param args ignored. for now. 
+   * Starts up the SensorBase web service.  Control-c to exit. 
+   * @param args Ignored. 
    */
-  public static void main(String[] args) {
-    try {
-        // Create a new Restlet component
-        Component component = new Component();
+  public static void main(String[] args) throws Exception {
+    System.out.println("Starting SensorBase server on port 9876.");
+    Component component = new Component();
+    component.getServers().add(Protocol.HTTP, 9876);
+    component.getDefaultHost().attach("/sensorbase", new Server() );
+    component.start();
+  }
 
-        // Create the HTTP server connector, then add it as a server
-        // connector to the Restlet component. Note that the component
-        // is the call restlet.
-        component.getServers().add(Protocol.HTTP, 9876);
-
-        // Prepare and attach a test Handler
-        Restlet handler = new Restlet(component.getContext()) {
-            @Override
-            public void handle(Request request, Response response) {
-                if (request.getMethod().equals(Method.PUT)) {
-                    System.out.println("Handling the call...");
-                    System.out
-                            .println("Trying to get the entity as a form...");
-                    Form form = request.getEntityAsForm();
-
-                    System.out.println("Trying to getParameters...");
-                    StringBuffer sb = new StringBuffer("foo");
-                    for (Parameter p : form) {
-                        System.out.println(p);
-
-                        sb.append("field name = ");
-                        sb.append(p.getName());
-                        sb.append("value = ");
-                        sb.append(p.getValue());
-                        sb.append("\n");
-                        System.out.println(sb.toString());
-                    }
-
-                    response.setEntity(sb.toString(), MediaType.TEXT_PLAIN);
-                    System.out.println("Done!");
-                } else {
-                    response.setStatus(Status.SERVER_ERROR_NOT_IMPLEMENTED);
-                }
-            }
-        };
-
-        component.getDefaultHost().attach("/test", handler);
-
-        // Now, start the component
-        component.start();
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
+  /**
+   * Dispatch to the Projects, SensorData, SensorDataTypes, or Users Resource depending on the URL.
+   */
+  @Override
+  public Restlet createRoot() {
+    Router router = new Router(getContext());
+    // This URI fragment specifies the "file" resource and a specific file name.
+    // The router will dispatch to the FileResource class for URLs with this template 
+    //router.attach("/file/{filename}", FileResource.class);
+    return router;
+  }
 }
-}
+
