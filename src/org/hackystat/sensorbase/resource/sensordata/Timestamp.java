7093ce4493456c9af58a6982e47f03028a817d0c
Philip Johnson <philipmjohnson@gmail.com>
2007-06-06 01:07:23 +0000

    [Issue 8] Finished basic Project resource implementation.  GET, PUT, DELETE all supported.  Must update REST API now.
    http://code.google.com/p/hackystat-sensorbase-uh/issues/detail?id=8

---
+++ src/org/hackystat/sensorbase/resource/sensordata/Timestamp.java
@@ -0,0 +1,127 @@
+package org.hackystat.sensorbase.resource.sensordata;
+
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConstants;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+/**
+ * Utility class that facilitates Timestamp representation and processing. 
+ * @author Philip Johnson
+ */
+public class Timestamp {
+  
+  /**
+   * Returns true if the passed string can be parsed into an XMLGregorianCalendar object.
+   * @param lexicalRepresentation The string representation.
+   * @return True if the string is a legal XMLGregorianCalendar. 
+   */
+  public static boolean isTimestamp(String lexicalRepresentation) {
+    try {
+      DatatypeFactory factory = DatatypeFactory.newInstance();
+      factory.newXMLGregorianCalendar(lexicalRepresentation);
+      return true;
+      
+    }
+    catch (Exception e) {
+      return false;
+    }
+  }
+  
+  /**
+   * Returns an XMLGregorianCalendar, given its string representation. 
+   * @param lexicalRepresentation The string representation.
+   * @return The timestamp. 
+   * @throws Exception If the string cannot be parsed into a timestamp. 
+   */
+  public static XMLGregorianCalendar makeTimestamp(String lexicalRepresentation) throws Exception {
+    DatatypeFactory factory = DatatypeFactory.newInstance();
+    return factory.newXMLGregorianCalendar(lexicalRepresentation);
+  }
+  
+  /**
+   * Returns an XMLGregorianCalendar corresponding to the current time.
+   * @return The timestamp. 
+   */
+  public static XMLGregorianCalendar makeTimestamp() {
+    try {
+      DatatypeFactory factory = DatatypeFactory.newInstance();
+      return factory.newXMLGregorianCalendar(new GregorianCalendar());
+    }
+    catch (Exception e) {
+      throw new RuntimeException("Bad datatypeFactory", e);
+    }
+  }
+  
+  /**
+   * Returns true if tstamp is equal to or between start and end.
+   * @param start The start time.
+   * @param end The end time.
+   * @param tstamp The timestamp to test. 
+   * @return True if between this interval.
+   */  
+  public static boolean inBetween(XMLGregorianCalendar start, XMLGregorianCalendar end, 
+      XMLGregorianCalendar tstamp) {
+    if ((start.compare(tstamp) == DatatypeConstants.EQUAL) ||
+        (end.compare(tstamp) == DatatypeConstants.EQUAL)) {
+      return true;
+    }
+    if ((start.compare(tstamp) == DatatypeConstants.LESSER) &&
+        (end.compare(tstamp) == DatatypeConstants.GREATER)) {
+      return true;
+    }
+    return false;
+  }
+  
+  /**
+   * Returns true if time1 > time2.
+   * @param time1 The first time. 
+   * @param time2 The second time. 
+   * @return True if time1 > time2
+   */
+  public static boolean greaterThan(XMLGregorianCalendar time1, XMLGregorianCalendar time2) {
+    return time1.compare(time2) == DatatypeConstants.GREATER;
+  }
+  
+  /**
+   * Returns true if timeString1 > timeString2.
+   * Throws an unchecked IllegalArgument exception if the strings can't be converted to timestamps.
+   * @param timeString1 The first time. 
+   * @param timeString2 The second time. 
+   * @return True if time1 > time2
+   */
+  public static boolean greaterThan(String timeString1, String timeString2) {
+    try {
+      DatatypeFactory factory = DatatypeFactory.newInstance();
+      XMLGregorianCalendar time1 = factory.newXMLGregorianCalendar(timeString1);
+      XMLGregorianCalendar time2 = factory.newXMLGregorianCalendar(timeString2);
+      return time1.compare(time2) == DatatypeConstants.GREATER;
+    }
+    catch (Exception e) {
+      throw new IllegalArgumentException("Illegal timestring", e);
+    }
+  }
+  
+  /**
+   * Returns true if time1 < time2.
+   * @param time1 The first time. 
+   * @param time2 The second time. 
+   * @return True if time1 < time2
+   */
+  public static boolean lessThan(XMLGregorianCalendar time1, XMLGregorianCalendar time2) {
+    return time1.compare(time2) == DatatypeConstants.LESSER;
+  }
+
+  /**
+   * Returns true if time1 equals time2
+   * @param time1 The first time. 
+   * @param time2 The second time. 
+   * @return True if time1 equals time2
+   */
+  public static boolean equal(XMLGregorianCalendar time1, XMLGregorianCalendar time2) {
+    return time1.compare(time2) == DatatypeConstants.EQUAL;
+  }
+  
+  
+}
